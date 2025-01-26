414a79d3f8b406eac6506f4569dd3e664e1a12d6
SomeSomeOne99 <zaydnadeem07@gmail.com>
2025-01-26 23:31:25 +0000

    Improve SIR mdoel gradient descent parameter training

--- sir.py
+++ sir.py
@@ -2,8 +2,7 @@ from base_model import Model # Import base Model class
 class SIR(Model):
     def __init__(self, infRate = None, recRate = None): # (infection rate, recovery rate) No initial weights by default
         self.infRate, self.recRate = infRate, recRate # Set initial weights if given
-    def train(self, x, y, iterationLimit = 10000): # Train model weights on given data with Newton-Raphson method
-        raise NotImplementedError("This method is not implemented") # Prevent use of incomplete method
+    def train(self, x, y, iterationLimit = 10000): # Train model weights on given data with gradient descent
         # Input type checks
         if type(x) != list or type(y) != list:
             raise TypeError("Data must be a list") # Invalid data type
@@ -11,34 +10,32 @@ class SIR(Model):
             raise ValueError("Data lists must be same length") # Data length mismatch
         if len(x) < 2:
             raise ValueError("Data must have two or more items")
-        # Train infection rate
-        self.infRate = (1 - y[1][0] / y[0][0]) ** (1 / (x[1] - x[0])) # Use first pair of points for initial estimation
-        self.recRate = 0.1 #y[2][2] / y[1][2] - 1
-        loss = sum([[(y[i][j] - x_)**2 for x_ in self.predict(x[i])] for i in range(len(x))]) / len(x) # Initial MSE loss
-        minLoss = float("inf")
+        # Train infRate with S and I data
+        self.infRate = 0.25 # Arbitrary initialisation
+        self.recRate = 0.3 # Arbitrary initialisation
+        minLoss = sum([sum([(y[i][j] - predY)**2 for j, predY in enumerate(self.predict(x[i])[:2])]) for i in range(len(x))]) # Initial loss for S and I data
         bestInfRate = self.infRate
-        iteration = 0
-        while loss > 0 and iteration < iterationLimit: # Continue until correct parameter value found or iteration limit reached
-            if minLoss > loss:
+        for _ in range(iterationLimit): # Continue until iteration limit reached
+            loss = sum([sum([(y[i][j] - predY)**2 for j, predY in enumerate(self.predict(x[i])[:2])]) for i in range(len(x))]) # New MSE loss
+            if loss == 0: # Perfect loss
+                return
+            self.infRate -= sum([(y[i - 1][0] * y[i - 1][1] * (y[i][0] - self.predict(x[i])[0]) * (y[i][1] - self.predict(x[i])[1])) for i in range(1, len(x))]) # Use gradient descent to minimise loss with S and I data
+            if loss < minLoss:
                 minLoss = loss
-                bestInfRate = self.infRate # Preserve best known value
-            self.infRate -= loss / (2 * sum([((-self.infRate * x[i - 1][0] * x[i - 1][1]) * (self.predict(x[i])[0] - y[i][0])) for i in range(len(1, x))])) # Train infection rate using Newton-Raphson method with S and I data
-            loss = sum([(y[i] - self.predict(x[i]))**2 for i in range(len(x))]) / len(x) # New MSE loss
-            iteration += 1
-        if loss > minLoss:
-            self.infRate = bestInfRate
-        loss = sum([(y[i] - self.predict(x[i]))**2 for i in range(len(x))]) / len(x) # Calculate initial MSE loss
-        minLoss = float("inf")
+                bestInfRate = self.infRate # Preserve best infRate
+        self.infRate = bestInfRate # Use best known infRate
+        # Train recRate with I and R data
+        minLoss = sum([sum([(y[i][j] - predY)**2 for j, predY in enumerate(self.predict(x[i])[1:])]) for i in range(len(x))]) # Initial loss for I and R data
         bestRecRate = self.recRate
-        iteration = 0
-        while loss > 0 and iteration < iterationLimit: # Continue until correct parameter value found or iteration limit reached
-            if minLoss > loss:
-                bestRecRate = self.recRate # Preserve best known value
-            self.recRate -= loss / (2 * () * sum([((self.recRate * x[i - 1][1] * x[i - 1][2]) * (self.predict(x[i]) - y[i])) for i in range(len(x))])) # Train infection rate using Newton-Raphson method with I and R data
-            loss = sum([(y[i] - self.predict(x[i]))**2 for i in range(len(x))]) / len(x) # Calculate new MSE loss
-            iteration += 1
-        if loss > minLoss:
-            self.recRate = bestRecRate
+        for _ in range(iterationLimit): # Continue until iteration limit reached
+            loss = sum([sum([(y[i][j] - predY)**2 for j, predY in enumerate(self.predict(x[i])[1:])]) for i in range(len(x))]) # New MSE loss
+            if loss == 0: # Perfect loss
+                return
+            self.recRate -= sum([(y[i - 1][1] * y[i - 1][2] * (y[i][1] - self.predict(x[i])[1]) * (y[i][2] - self.predict(x[i])[2])) for i in range(1, len(x))]) # Use gradient descent to minimise loss with I and R data
+            if loss < minLoss:
+                minLoss = loss
+                bestRecRate = self.recRate # Preserve best recRate
+        self.recRate = bestRecRate # Use best known recRate
     def train_naive(self, x, y, initialPrecision = -1, finalPrecision = -15): # Alternative naive iteration algorithm
         # Input type checks
         if type(x) != list or type(y) != list:
@@ -93,8 +90,11 @@ class SIR(Model):
 sirModel = SIR()
 targetModel = SIR(0.5, 0.25)
 print("train")
-sirModel.train([x for x in range(100)], [targetModel.predict(x) for x in range(100)]) # Train with Newton-Raphson method
-print(sirModel.predict(10))
+sirModel.train([x for x in range(25)], [targetModel.predict(x) for x in range(25)]) # Train with Newton-Raphson method
+for testX in [0, 10, 100, 1000]:
+    print(sirModel.predict(testX), "->", targetModel.predict(testX)) # Predict for given inputs
+print(sirModel.infRate, "->", targetModel.infRate)
+print(sirModel.recRate, "->", targetModel.recRate)
 print("train_naive")
 sirModel.train_naive([x for x in range(100)], [targetModel.predict(x) for x in range(100)]) # Train with naive iterative method (clears past learning)
 for testX in [0, 10, 100, 1000]:
