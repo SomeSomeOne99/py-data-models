cde399d552305901ccbd4d58baa29958aace6db4
SomeSomeOne99 <zaydnadeem07@gmail.com>
2025-01-28 22:17:06 +0000

    Refactor method parameters in base Model class to match specific implementations

--- base_model.py
+++ base_model.py
@@ -1,9 +1,9 @@
 class Model(): # General model class
     def __init__(self):
         pass
-    def train(self, data): # Train parameters of model to given data
+    def train(self, x, y): # Train parameters of model to given data
         pass
-    def predict(self, input): # Return model prediction for given input
+    def predict(self, x): # Return model prediction for given input
         pass
     def loss(self, x, y):
         return sum([(y[i] - self.predict(x[i]))**2 for i in range(len(x))]) / len(x)
