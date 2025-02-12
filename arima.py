8b570b24bf18ec6a3fb3156df09d00fe10d99242
SomeSomeOne99 <zaydnadeem07@gmail.com>
2025-02-12 23:42:43 +0000

    Create .difference and .reverse_difference methods to process raw data to/from differenced series

--- arima.py
+++ arima.py
@@ -4,25 +4,28 @@ class ARIMA(Model): # General model class
         self.const, self.ar_coef, self.diff, self.ma_coef = 0, [0], 0, [0] # Initialize coefficients
     #def train(self, y): # Train parameters of model to given data
     #    pass
-    def predict(self, x, forecasts = 10, forecasts_only = True): # Return model prediction for given inputs list
-        def remove_differencing(x_diff, initial):
+    def difference(self, x, d): # Apply differencing to input list
+        x_lasts = [] # Store last values for reverse differencing
+        for _ in range(d):
+            x_lasts.append(x[-1]) # Store last value
+            x = [x[i] - x[i - 1] for i in range(1, len(x))] # Apply differencing
+        return x, x_lasts
+    def reverse_difference(self, x_diff, x_lasts): # Reverse differencing of input list
+        def remove_differencing_step(x_diff, initial):
             x = [initial] # Start at initial value
             for i in range(len(x_diff)):
                 x.append(x[-1] + x_diff[i]) # Reverse differencing
             return x[1:] # Remove initial value
-        # Apply differencing
-        x_lasts = [] # Store last values for reverse differencing
-        for _ in range(self.diff):
-            x_lasts.append(x[-1]) # Store last value
-            x = [x[i] - x[i - 1] for i in range(1, len(x))]
+        for d in range(self.diff):
+            x_diff = remove_differencing_step(x_diff, x_lasts[-1 - d]) # Reverse differencing
+        return x_diff
+    def predict(self, x, forecasts = 10, forecasts_only = True): # Return model prediction for given inputs list
+        x, x_lasts = self.difference(x, self.diff) # Apply differencing
         predictions_diff = []
         for i in range(len(x) + forecasts):
             predictions_diff.append(self.const + sum([self.ar_coef[j] * (predictions_diff[i - j] if i - j >= len(x) else x[i - j]) for j in range(1, len(self.ar_coef)) if i - j >= 0]) + sum([self.ma_coef[j] * (predictions_diff[i - j] - x[i - j]) for j in range(1, len(self.ma_coef)) if 0 <= i - j < len(x)]))
-        # Apply inverse differencing
         if forecasts > 0:
-            forecasts = predictions_diff[-forecasts:] # Separate forecasts
-            for d in range(self.diff):
-                forecasts = remove_differencing(forecasts, x_lasts[-1 - d]) # Reverse differencing
+            forecasts = self.reverse_difference(predictions_diff[-forecasts:], x_lasts) # Separate forecasts and invert differencing
         return ([] if forecasts_only else (predictions_diff[:-forecasts] if forecasts > 0 else predictions_diff)) + forecasts
     def loss(self, x):
         predictions = self.predict(x, forecasts = 0, forecasts_only = False)
